2246beec4ea1723b18d60e56912b147e7e098241
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-21 17:57:10 +0300

    retrieved data from MainActivity

--- app/src/main/java/com/moringaschool/myrestaurants/RestrauntActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/RestrauntActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.myrestaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class RestrauntActivity extends AppCompatActivity {
@@ -9,6 +10,8 @@ public class RestrauntActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restraunt);
+        setContentView(R.layout.activity_restaurant);
+        Intent intent = getIntent();
+        String locationString = intent.getStringExtra("location");
     }
 }
