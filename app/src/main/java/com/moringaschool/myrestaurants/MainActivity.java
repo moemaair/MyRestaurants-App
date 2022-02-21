e114f81b04169d53964bb05361fa2983d098f7b7
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-21 16:58:16 +0300

    implemented Intents to navigate to activity_restaurant.xml

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -2,13 +2,27 @@ package com.moringaschool.myrestaurants;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mFindRestaurantsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "welcome to restaurant",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestrauntActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
