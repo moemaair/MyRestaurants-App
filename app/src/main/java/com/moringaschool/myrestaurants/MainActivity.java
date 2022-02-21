f9d73d46b2b7c160bc99284867f6fd88f57b0646
Mohamed A. Ibrahim <rageali12@gmail.com>
2022-02-21 17:47:47 +0300

    Gathering input from EditText

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -3,26 +3,42 @@ package com.moringaschool.myrestaurants;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //member variable or instance variables
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mLocationEditText =(EditText) findViewById(R.id.locationEditText);
+
+        //click event
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Toast.makeText(MainActivity.this, "welcome to restaurant",Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestrauntActivity.class);
                 startActivity(intent);
+                Toast.makeText(MainActivity.this, location,Toast.LENGTH_LONG).show();
+
+
             }
         });
+
     }
 }
